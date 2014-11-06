c7b6b785270cb7c442e9717ce5b300ac3b16a62b
Garry Yao <yaojun85@gmail.com>
2014-11-06 11:43:35 +0800

    introduce the new troopjs adaptive widget for hooking in react component

---
+++ widget/react.js
@@ -0,0 +1,20 @@
+define([
+  'troopjs-dom/component/widget',
+  'when',
+  'react',
+  'require'
+], function (Widget, when, React, require) {
+  return Widget.extend(function ($element, displayName, module, props) {
+    this.boot = function () {
+      return when.promise(function (resolve) {
+        require([module], function (Component) {
+          React.renderComponent(Component(props), $element, resolve);
+        });
+      });
+    }
+  }, {
+    'sig/start': function reactWidgetStart() {
+      return this.boot();
+    }
+  });
+});
